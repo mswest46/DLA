22432d2eac5136daee7821cfba9ac56e7397d3a3
Michael West <mswest46@gmail.com>
2016-08-17 22:30:39 -0400

    make testDLA file

---
+++ src/testDLA.java
@@ -0,0 +1,7 @@
+public class testDLA {
+  private static final int nNodes = 10;
+  public static void main(String[] args) {
+    DLA aggregate = new DLA(nNodes);
+    System.out.print(aggregate.toString()); 
+  }
+}
